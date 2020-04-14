fcfead8aa4f2b1288e94e34ba9fd6969cd94a0fe
Dima Chuchukalo <Dimachuchukalo@gmail.com>
2020-04-14 14:48:31 +0300

    fixed some mistakes

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -1,50 +1,51 @@
 import timerTemplates from '../templates/timer.hbs';
 
-let id = null;
-
-const bodyRef = document.querySelector('body');
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
- function refreshClock(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  const timeFormules = [days, hours, mins, secs];
-
-  const timeRefs = {
-    daysRef: document.querySelector(`#${id} [data-value="days"]`),
-    hoursRef: document.querySelector(`#${id} [data-value="hours"]`),
-    minsRef: document.querySelector(`#${id} [data-value="mins"]`),
-    secsRef: document.querySelector(`#${id} [data-value="secs"]`),
-  };
-
-  let i = 0;
-  for (const key in timeRefs) {
-    timeRefs[key].textContent = [...timeFormules[i]];
-    i += 1;
-  }
-}
-
 export default class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
   }
-  
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  refreshClock(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    const timeFormules = [days, hours, mins, secs];
+    const id = this.selector;
+    const timeRefs = {
+      daysRef: document.querySelector(`#${id} [data-value="days"]`),
+      hoursRef: document.querySelector(`#${id} [data-value="hours"]`),
+      minsRef: document.querySelector(`#${id} [data-value="mins"]`),
+      secsRef: document.querySelector(`#${id} [data-value="secs"]`),
+    };
+
+    let i = 0;
+    for (const key in timeRefs) {
+      timeRefs[key].textContent = [...timeFormules[i]];
+      i += 1;
+    }
+  }
+
+  drawInterface() {
+    const deltaTime = this.targetDate - Date.now();
+    this.refreshClock(deltaTime);
+  }
+
   create() {
     const marckup = timerTemplates(this.selector);
-    setTimeout(() => {
-      bodyRef.insertAdjacentHTML('beforeend', marckup);
-    }, 1000);
+    const bodyRef = document.querySelector('body');
+    bodyRef.insertAdjacentHTML('beforeend', marckup);
+    this.drawInterface();
     setInterval(() => {
-      const deltaTime = this.targetDate - Date.now();
-      id = this.selector;
-      refreshClock(deltaTime);
+      this.drawInterface();
     }, 1000);
   }
 }
